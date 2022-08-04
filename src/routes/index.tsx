47f55ea4062e25102a5bb1a23f02b8e440bfa4c4
neivaldo moraes <neivaldo.moraes@pti.org.br>
2022-08-04 16:38:15 -0300

    feat: adicionado react-router-dom

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,10 @@
-export {};
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+export function AppRoutes () {
+	return (
+		<Routes>
+			<Route path="/pagina-inicial" element={<p>Sup</p>} />
+			<Route path="*" element={<Navigate to="/pagina-inicial" />} />
+		</Routes>
+	)
+}
